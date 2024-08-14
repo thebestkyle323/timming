9014501a64eca1d9368477ff6121d151bbc9caab
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-08-14 09:11:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,23 +14,22 @@ const TRENDING_URL =
   'https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot';
 const TRENDING_DETAIL_URL = 'https://m.s.weibo.com/topic/detail?q=%s';
 
+// 提供一个固定的图片链接
+const IMAGE_URL = 'https://app.iwanshare.club/uploads/20240814/cf643ec476d0a9afff266f7a18695bea.jpg'; // 替换成实际的图片链接
+
 const bot = new Telegraf(TOKEN);
 
 let RETRY_TIME = 5;
 
 async function sendTgMessage(data) {
   const ranks = [
-    '0️⃣1️⃣', '0️⃣2️⃣', '0️⃣3️⃣', '0️⃣4️⃣', '0️⃣5️⃣', '0️⃣6️⃣', '0️⃣7️⃣', '0️⃣8️⃣', '0️⃣9️⃣', '1️⃣0️⃣',
-    '1️⃣1️⃣', '1️⃣2️⃣', '1️⃣3️⃣', '1️⃣4️⃣', '1️⃣5️⃣', '1️⃣6️⃣', '1️⃣7️⃣', '1️⃣8️⃣', '1️⃣9️⃣', '2️⃣0️⃣',
-    '2️⃣1️⃣', '2️⃣2️⃣', '2️⃣3️⃣', '2️⃣4️⃣', '2️⃣5️⃣', '2️⃣6️⃣', '2️⃣7️⃣', '2️⃣8️⃣', '2️⃣9️⃣', '3️⃣0️⃣',
-    '3️⃣1️⃣', '3️⃣2️⃣', '3️⃣3️⃣', '3️⃣4️⃣', '3️⃣5️⃣', '3️⃣6️⃣', '3️⃣7️⃣', '3️⃣8️⃣', '3️⃣9️⃣', '4️⃣0️⃣',
-    '4️⃣1️⃣', '4️⃣2️⃣', '4️⃣3️⃣', '4️⃣4️⃣', '4️⃣5️⃣', '4️⃣6️⃣', '4️⃣7️⃣', '4️⃣8️⃣', '4️⃣9️⃣', '5️⃣0️⃣'
+    '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.', '11.', '12.', '13.', '14.', '15.', '16.', '17.', '18.', '19.', '20.', '21.', '22.', '23.', '24.', '25.'
   ];
 
   // 过滤掉带有推广的信息
   const filteredData = data.filter(o => !o.promotion);
 
-  const text = filteredData.splice(1, 50).map((o, i) => {
+  const text = filteredData.splice(1, 25).map((o, i) => {
     const containerid = encodeURIComponent(
       new URL(o.scheme).searchParams.get('containerid'),
     );
@@ -48,19 +47,19 @@ async function sendTgMessage(data) {
   text.unshift(
     `**微博实时热搜** ${dayjs().format(
       'YYYY-MM-DD HH:mm:ss',
-    )} ([查看更多]())\n`,
+    )} ([查看更多]('https://nav.iosfans.club/'))\n`,
   );
-  
-  // 发送消息到第一个频道
-  await bot.telegram.sendMessage(CHANNEL_ID_1, text.join('\n'), {
+
+  // 将图片和文本一起发送到第一个频道
+  await bot.telegram.sendPhoto(CHANNEL_ID_1, IMAGE_URL, {
+    caption: text.join('\n'),
     parse_mode: 'Markdown',
-    disable_web_page_preview: true,
   });
 
-  // 发送消息到第二个频道
-  await bot.telegram.sendMessage(CHANNEL_ID_2, text.join('\n'), {
+  // 将图片和文本一起发送到第二个频道
+  await bot.telegram.sendPhoto(CHANNEL_ID_2, IMAGE_URL, {
+    caption: text.join('\n'),
     parse_mode: 'Markdown',
-    disable_web_page_preview: true,
   });
 }
 
